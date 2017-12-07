e2aa5a5e64968b348b86518d4eaedf5a194008a3
Mark Knol <mark.knol@gmail.com>
2017-12-07 21:59:01 +0100

    Really build samples

---
+++ src/BuildSamples.hx
@@ -0,0 +1,60 @@
+package;
+
+import sys.FileSystem;
+import sys.io.File;
+import sys.io.Process;
+
+/**
+ * @author Mark Knol
+ */
+class BuildSamples
+{
+	//static var heapsVersionTag = "1.0.0";
+	//static var heapsFolder = "C:/HaxeToolkit/libs/heaps/git";
+	static var TEMPFILE = ".install-heaps.tmp";
+	static var FOLDER = ".temp/";
+	
+	static function main() 
+	{
+		var sampleDir = FOLDER + "samples/";
+		var sampleBuildDir = sampleDir + "/build/";
+		trace( "** Git the samples");
+		Sys.command('git',["clone", "--branch",  "1.1.0", "--depth", "1", "--single-branch", "https://github.com/HeapsIO/heaps.git", FOLDER]);
+		
+		trace( "** Build the samples");
+		
+		Sys.setCwd(sampleDir);
+		Sys.command('haxe',["all.hxml"]);
+		
+		trace( "** Copy the samples");
+		Sys.setCwd("./");
+		copyDirectory(sampleBuildDir, "assets/includes/samples/");
+		
+		trace( "** Samples done!");
+	}
+	
+	static function runCommand(cmd:String, ?args:Array<String>):String
+	{
+		var p = new Process(cmd, args);
+		var out:String = p.stdout.readAll().toString();
+		p.close();
+		//File.saveContent(TEMPFILE, out);
+		return out;
+	}
+	
+	static function copyDirectory(dir:String, path:String) {
+		FileSystem.createDirectory(path);
+		trace("include directory: " + path);
+		
+		for (file in FileSystem.readDirectory(dir)) {
+			var srcPath = '$dir/$file';
+			var dstPath = '$path/$file';
+			if (FileSystem.isDirectory(srcPath)) {
+				FileSystem.createDirectory(dstPath);
+				copyDirectory(srcPath, dstPath);
+			} else {
+				File.copy(srcPath, dstPath);
+			}
+		}
+	}
+}
