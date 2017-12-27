3d1625d4c4cb27046027209f34c7e2c14946c053
Andy Li <andy@onthewings.net>
2017-12-27 13:40:51 +0800

    We need to unshallow the git repo before deploy.

--- src/DeployGhPages.hx
+++ src/DeployGhPages.hx
@@ -6,6 +6,11 @@ using StringTools;
 class DeployGhPages {
     static function main():Void {
         var root = getCwd();
+
+        // TravisCI by default clones repositories to a depth of 50 commits.
+        // We need a full clone inorder to do git operations.
+        runCommand("git", ["fetch", "--unshallow"]);
+
         var sha = commandOutput("git", ["rev-parse", "HEAD"]).trim();
 
         setCwd(htmlDir);
