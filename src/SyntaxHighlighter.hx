7c66eb597fa04b2bb5d4bc6516f861c040614eeb
Phil Chertok <pchertok@gmail.com>
2018-06-15 19:09:48 -0400

    Added syntax highlighting - run 'npm install' and 'git submodules init'

---
+++ src/SyntaxHighlighter.hx
@@ -0,0 +1,39 @@
+import haxe.Json;
+import highlighter.Highlighter;
+import sys.io.File;
+
+class SyntaxHighlighter
+{
+	public static function patch () {
+		Sys.println("Applying syntax highlighting ...");
+
+		var haxeGrammar = new Highlighter("grammars/haxe-TmLanguage/haxe.tmLanguage");
+		var hxmlGrammar = new Highlighter("grammars/haxe-TmLanguage/hxml.tmLanguage");
+		
+		var grammars = [
+			"haxe" => haxeGrammar,
+			"hxml" => hxmlGrammar,			
+		];
+
+		// Go over the generated HTML file and apply syntax highlighting
+		var missingGrammars = Highlighter.patchFolder("output", grammars, function (classList) {
+			return classList.substr(12);
+		});
+
+		for (g in missingGrammars) {
+			Sys.println('Missing grammar for "${g}"');
+		}
+
+		// Add CSS rules for highlighting
+		var path = "output/css/styles.min.css";
+		var baseStyle = File.getContent(path);
+		var syntaxStyle = haxeGrammar.runCss();
+		File.saveContent(path, baseStyle + syntaxStyle);
+	}
+
+	public static function main(){
+		SyntaxHighlighter.patch();
+	}
+}
+
+
