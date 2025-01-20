8e982a9cecd15d7254780315a886ecac6fb3e390
tobil4sk <tobil4sk@outlook.com>
2025-01-20 11:02:58 +0000

    Update highlighter library (#74)
    * Update highlighter library
    
    * [ci] Use provided version of node
    
    Since highlighter has now been updated, we no longer need to use an old
    version of node.
    
    * Remove version constraint on highlighter library

--- src/SyntaxHighlighter.hx
+++ src/SyntaxHighlighter.hx
@@ -7,40 +7,37 @@ class SyntaxHighlighter
 	public static function patch () {
 		Sys.println("Applying syntax highlighting ...");
 
-		var haxeGrammar = new Highlighter("grammars/haxe-TmLanguage/haxe.tmLanguage");
-		var hxmlGrammar = new Highlighter("grammars/haxe-TmLanguage/hxml.tmLanguage");
-		var htmlGrammar = new Highlighter("grammars/html.tmbundle/Syntaxes/HTML.plist");
-
-		var grammars = [
-			"haxe" => haxeGrammar,
-			"hxml" => hxmlGrammar,
-			"html" => htmlGrammar,
+		var grammarFiles = [
+			"haxe" => "grammars/haxe-TmLanguage/haxe.tmLanguage",
+			"hxml" => "grammars/haxe-TmLanguage/hxml.tmLanguage",
+			"html" => "grammars/html.tmbundle/Syntaxes/HTML.plist",
 		];
 
-		// Go over the generated HTML file and apply syntax highlighting
-		var missingGrammars = Highlighter.patchFolder("output/documentation", grammars, function (classList) {
-			return classList.substr(12);
+		Highlighter.loadHighlighters(grammarFiles, function(highlighters) {
+			// Go over the generated HTML file and apply syntax highlighting
+			var missingGrammars = Highlighter.patchFolder("output/documentation", highlighters, function(classList) {
+				return classList.substr(12);
+			});
+
+			for (g in missingGrammars) {
+				Sys.println('Missing grammar for "${g}"');
+			}
+
+			// Go over the generated HTML file and apply syntax highlighting
+			missingGrammars = Highlighter.patchFolder("output/samples", highlighters, function(classList) {
+				return classList.substr(12);
+			});
+
+			for (g in missingGrammars) {
+				Sys.println('Missing grammar for "${g}"');
+			}
+
+			// Add CSS rules for highlighting
+			var path = "output/css/styles.min.css";
+			var baseStyle = File.getContent(path);
+			var syntaxStyle = highlighters["haxe"].runCss();
+			File.saveContent(path, baseStyle + syntaxStyle);
 		});
-
-		for (g in missingGrammars) {
-			Sys.println('Missing grammar for "${g}"');
-		}
-
-		// Go over the generated HTML file and apply syntax highlighting
-		missingGrammars = Highlighter.patchFolder("output/samples", grammars, function (classList) {
-			return classList.substr(12);
-		});
-
-		for (g in missingGrammars) {
-			Sys.println('Missing grammar for "${g}"');
-		}
-
-
-		// Add CSS rules for highlighting
-		var path = "output/css/styles.min.css";
-		var baseStyle = File.getContent(path);
-		var syntaxStyle = haxeGrammar.runCss();
-		File.saveContent(path, baseStyle + syntaxStyle);
 	}
 
 	public static function main(){
